f1498c68e7873f3245714879c9b937942fb0e515
tama926 <acta_tamami@icloud.com>
2020-07-05 20:22:56 +0900

    GoForwardUnityChan 課題
    レッスン７

---
+++ Assets/SE.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SE : MonoBehaviour {
+
+    public AudioClip sound1;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        //Componentを取得
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update () {
+            
+        }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "CubeTag" || other.gameObject.tag == "ZimenTag")
+        {
+            audioSource.PlayOneShot(sound1);
+
+        }
+    }
+
+    }
+	
